9ba688db1d6f01ef412b67deeea742fcf33b70bd
jjakov19 <jovislav2002@gmail.com>
2025-09-14 22:28:31 +0200

    Sve odradjeno fali debug u templejtskoj klasi.

--- Lezaljka.h
+++ Lezaljka.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class Lezaljka
 {
+	int brIznajmljenih;
+	int cenaPica;
+public:
+	Lezaljka():brIznajmljenih(0),cenaPica(0){}
+	Lezaljka(int brIz, int cena) :brIznajmljenih(brIz), cenaPica(cena) {}
+	Lezaljka(const Lezaljka& l) :brIznajmljenih(l.brIznajmljenih), cenaPica(l.cenaPica) {}
+	~Lezaljka(){}
+	int getBrIzn()const { return brIznajmljenih; }
+	int getCenaPica()const { return cenaPica; }
+	friend ostream& operator <<(ostream& os, Lezaljka& l) {
+		os << l.brIznajmljenih << " " << l.cenaPica << " " << endl;
+		return os;
+	}
+	friend istream& operator>>(istream& is, Lezaljka& l) {
+		is >> l.brIznajmljenih >> l.cenaPica;
+		return is;
+	}
 };
-
--- Plaza.h
+++ Plaza.h
@@ -1,5 +1,212 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "Lezaljka.h"
+
 template<typename T>
 class Plaza {
-
+	T** matrica;
+	int trbr;
+	int brRedova;
+	int brKol;
+public:
+	Plaza();
+	Plaza(int brRed,int brKoll);
+	Plaza(const Plaza& p);
+	~Plaza();
+	void Dodaj(int rd, int kl, T lez);
+	void Oslobodi(int rd, int kl);
+	friend ostream& operator<<(ostream& os, Plaza<T>& l) {
+		l.Print(os);
+		return os;
+	}
+	void Ispisi(ostream& os) {
+		for (int i = 0; i < brRedova; i++) {
+			for (int j = 0; j < brKol; j++) {
+				os << matrica[i][j] << " ";
+			}
+			os << endl;
+		}
+		return os;
+	}
+	ostream& Print(ostream& os) {
+		for (int i = 0; i < brRedova; i++) {
+			for (int j = 0; j < brKol; j++) {
+				os << matrica[i][j] << " ";
+			}
+			os << endl;
+		}
+		return os;
+	}
+	float Ukupno();
+	int BrojMesta(int brlez);
+	int RedSaNajvise(int brlez);
+	void Sacuvaj(const char* fajl);
+	void Ucitaj(const char* fajl);
 };
+template <typename T>
+Plaza<T>::Plaza() {
+	trbr = 0;
+	brRedova = 0;
+	brKol = 0;
+	matrica = nullptr;
+}
+
+template <typename T>
+Plaza<T>::Plaza(int brRed, int brKoll) {
+	trbr = 0;
+	brRedova = brRed;
+	brKol = brKoll;
+	matrica = new T*[brRed];
+	for (int i = 0; i < brRedova; i++) {
+		matrica[i] = new T[brKol];
+	}
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			matrica[i][j] = T();
+		}
+	}
+}
+template <typename T>
+Plaza<T>::Plaza(const Plaza& p){
+	trbr = p.trbr;
+	brRedova = p.brRedova;
+	brKol = p.brKol;
+	matrica = new T * [brRedova];
+	for (int i = 0; i < brRedova; i++) {
+		matrica[i] = new T[brKol];
+	}
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			matrica[i][j] = p.matrica[i][j];
+		}
+	}
+}
+template <typename T>
+Plaza<T>::~Plaza() {
+	for (int i = 0; i < brRedova; i++) {
+		delete matrica[i];
+	}
+	delete[]matrica;
+}
+template <typename T>
+void Plaza<T>::Dodaj(int rd, int kl, T lez) {
+	if (rd > brRedova || kl > brKol || rd<0|| kl<0 ) {
+		throw runtime_error("Uneli ste nepostojecu poziciju!");
+		return;
+	}
+	matrica[rd][kl] = lez;
+}
+template <typename T>
+void Plaza<T>::Oslobodi(int rd, int kl) {
+	if (rd > brRedova || kl > brKol || rd < 0 || kl < 0) {
+		throw runtime_error("Uneli ste nepostojecu poziciju!");
+		return;
+	}
+	matrica[rd][kl] = T();
+}
+
+template <typename T>
+float Plaza<T>::Ukupno() {
+	float uk = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			uk += matrica[i][j];
+		}
+	}
+	return uk;
+}
+template <>
+float Plaza<Lezaljka>::Ukupno() {
+	float uk = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			uk += matrica[i][j].getBrIzn();
+		}
+	}
+	return uk;
+}
+template <typename T>
+int Plaza<T>::BrojMesta(int brlez) {
+	int uk = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			if(matrica[i][j]==brlez)
+			uk += matrica[i][j];
+		}
+	}
+	return uk;
+}
+template <>
+int Plaza<Lezaljka>::BrojMesta(int brlez) {
+	int uk = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			if (matrica[i][j].getBrIzn() == brlez)
+				uk += matrica[i][j].getBrIzn();
+		}
+	}
+	return uk;
+}
+template <typename T>
+int Plaza<T>::RedSaNajvise(int brlez) {
+	int maxRed = 0;
+	int maxRedSuma =0;
+	int trRedSuma = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			trRedSuma += matrica[i][j];
+		}
+		if (trRedSuma > maxRedSuma) {
+			maxRedSuma = trRedSuma;
+			maxRed = i;
+		}
+		trRedSuma = 0;
+	}
+	return maxRed;
+}
+template <>
+int Plaza<Lezaljka>::RedSaNajvise(int brlez) {
+	int maxRed = 0;
+	int maxRedSuma = 0;
+	int trRedSuma = 0;
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			trRedSuma += matrica[i][j].getBrIzn();
+		}
+		if (trRedSuma > maxRedSuma) {
+			maxRedSuma = trRedSuma;
+			maxRed = i;
+		}
+		trRedSuma = 0;
+	}
+	return maxRed;
+}
+
+template <typename T>
+void Plaza<T>::Sacuvaj(const char* fajl) {
+	ofstream file(fajl);
+	if (!file.good()) {
+		throw runtime_error("Lose ucitan file!");
+		return;
+	}
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			file << matrica[i][j];
+		}
+	}
+}
+template <typename T>
+void Plaza<T>::Ucitaj(const char* fajl) {
+	ifstream file(fajl);
+	if (!file.good()) {
+		throw runtime_error("Lose ucitan file!");
+		return;
+	}
+	for (int i = 0; i < brRedova; i++) {
+		for (int j = 0; j < brKol; j++) {
+			file >> matrica[i][j];
+		}
+	}
+}
